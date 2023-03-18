406db8bab51bdfe6655d6689fa063b2015bf9147
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-18 13:15:28 -0300

    service layer, component registration

--- src/main/java/com/pedrodelfino/course/repositories/UserRepository.java
+++ src/main/java/com/pedrodelfino/course/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.pedrodelfino.course.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pedrodelfino.course.entities.User;
