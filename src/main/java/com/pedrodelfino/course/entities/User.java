ee7e83e3b7e7164e9b516fd98ee73110654dae06
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-23 22:26:15 -0300

    Order, Instant, ISO8601

--- src/main/java/com/pedrodelfino/course/entities/User.java
+++ src/main/java/com/pedrodelfino/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.pedrodelfino.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -31,6 +37,10 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Long getId() {
