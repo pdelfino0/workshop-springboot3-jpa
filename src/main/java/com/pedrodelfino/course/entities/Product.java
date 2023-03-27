62238efc910bf7f78f910dc829c0876f94530753
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-27 20:44:30 -0300

    OrderItem, many-to-many association with extra attributes part 1

--- src/main/java/com/pedrodelfino/course/entities/Product.java
+++ src/main/java/com/pedrodelfino/course/entities/Product.java
@@ -12,9 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_products")
