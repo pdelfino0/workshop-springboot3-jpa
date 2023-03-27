e8c1342c79ee21c8d77d92742587285fa7ee0b39
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-27 18:53:35 -0300

    Order, Instant, ISO8601 part 2

--- src/main/java/com/pedrodelfino/course/entities/Order.java
+++ src/main/java/com/pedrodelfino/course/entities/Order.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,23 +18,24 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable {	
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
-	
-	public Order(){
-		
+	public Order() {
+
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -78,6 +83,5 @@ public class Order implements Serializable {
 		this.moment = moment;
 		this.client = client;
 	}
-
 
 }
