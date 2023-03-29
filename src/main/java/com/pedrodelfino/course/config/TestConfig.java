3e6bfc5642c51c259b99d08156f134b81643e4f6
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-28 22:17:50 -0300

    payment one-to-one association

--- src/main/java/com/pedrodelfino/course/config/TestConfig.java
+++ src/main/java/com/pedrodelfino/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedrodelfino.course.entities.Category;
 import com.pedrodelfino.course.entities.Order;
 import com.pedrodelfino.course.entities.OrderItem;
+import com.pedrodelfino.course.entities.Payment;
 import com.pedrodelfino.course.entities.Product;
 import com.pedrodelfino.course.entities.User;
 import com.pedrodelfino.course.entities.enums.OrderStatus;
@@ -84,6 +85,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 	
 	
--- src/main/java/com/pedrodelfino/course/entities/Order.java
+++ src/main/java/com/pedrodelfino/course/entities/Order.java
@@ -9,13 +9,16 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pedrodelfino.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,6 +41,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 
@@ -89,6 +95,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items; 
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -106,5 +121,6 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 }
